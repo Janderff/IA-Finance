d5d4bbb628bef8d627cfd92009fb7b791889e8b7
Janderff <janderffigueiredo@gmail.com>
2025-07-23 22:41:56 -0300

    feat(controllers):Finalização dos controllers

---
+++ server/src/http/routes/controllers/get-recent-transactions.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { prisma } from '../../../lib/prisma.ts'
+
+export async function getRecentTransactions(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const transactions = await prisma.transaction.findMany({
+      orderBy: {
+        date: 'desc',
+      },
+      take: 4,
+    })
+    return reply.status(200).send({ transactions })
+  } catch (error) {
+    if (error) {
+      console.log(error)
+    }
+  }
+}
